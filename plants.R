bbd120a0e10986741d68a987dfc17d37006bcbcb
jocelyn-cuthbert <jcuthber@aggiemail.usu.edu>
2016-11-18 16:40:04 -0700

    baby steps = still not working

--- plants.R
+++ plants.R
@@ -1,19 +1,19 @@
 #notes
+#This is a random number generator...I'm going to go ahead and just define the cells of the matrix
+#p_mat <- replicate(10,rnorm(10,rnorm(1),runif(1, min=0, max=1)))
+#plants_happy_home <- abs(p_mat)
+#plants_happy_home
 
 mat_function <- function (x, y){
   mat <- matrix(ncol=x, nrow=y)
   return(mat)
 }
 
-mat <- mat_function(10,10)
-
-p_mat <- replicate(10,rnorm(10,rnorm(1),runif(1, min=0, max=1)))
-plants_happy_home <- abs(p_mat)
-plants_happy_home
-
-#This seems like a good start, but not exactly what I need, so I manually made what I
-#needed to start.
+#mat <- mat_function(3,3)
+#mat
+#Don't think I need this - 
 ######need to define cell, so will make a new terrain matrix of numbers and NA
+
 t_mat <- mat_function(3,3)
 t_mat[1,1] <- 5.32
 t_mat[2,1] <- 0.73
@@ -26,11 +26,14 @@ t_mat[2,2] <- 0.59
 t_mat[3,3] <- 5.05
 t_mat
 
-#Repro = vector, same length as matrix, prob. a member of that species will reproduce
-repro500 <- c(.6,.2,.3)
-repro500
+#reproductionfunction = vector, same length as matrix, prob. a member of that species will reproductionfunctionduce
+
+reproductionfunction <- c(.6,.2,.3)
+reproductionfunction
+
 #comp.mat = dim = # plant species, prob. species will survive when in comes into contact with
 #another species.
+
 c_mat <- mat_function(3,3)
 c_mat[1,1] <- .5
 c_mat[2,1] <- .7
@@ -41,27 +44,28 @@ c_mat[2,3] <- .73
 c_mat[3,2] <- .02
 c_mat[2,2] <- .5
 c_mat[3,3] <- .5
+c_mat
 
 #survive = probability (0-1)
-survive_plants <- c(.9,.3,.4)
-#so now I am getting names, need to go ahead and establish that
-names <- c("weed", "flower", "grass")
-rownames(c_mat) <-names
-colnames(c_mat) <- names
-#plant things
 
-setup.plants <- function (repro, survive, comp.mat, names=c("weed","flower","grass")){
-  if (length(repro) !=length (survive))
-    stop ("Reproduction and survival parameters needed for all species")
-  if (length(repro) !=nrow (c_mat))
-    stop ("Reproduction and comp parameters needed for all species")
+live_plants_live <- c(.9,.3,.4)
+live_plants_live
+
+#need to setup my pretty pretty garden
+
+setup.plants <- function (reproductionfunction, live_plants_live, c_mat, names=c("weed","flower","grass")){
+  if (length(reproductionfunction) !=length (live_plants_live))
+    stop ("reproductionfunctionduction and survival parameters needed for all species")
+  if (length(reproductionfunction) !=nrow (c_mat))
+    stop ("reproductionfunctionduction and comp parameters needed for all species")
   if (ncol(c_mat) != nrow(c_mat))
     stop ("comp needs row and column to match")
-  repro <- setNames (repro,names)
-  return(list(repro=repro, survive=survive, c_mat=c_mat, names=names))
+  reproductionfunction <- setNames (reproductionfunction,names)
+  return(list(reproductionfunction=reproductionfunction, live_plants_live=live_plants_live, c_mat=c_mat, names=names))
 }
+#ok....everything has worked up to here
 
-info <- setup.plants(repro500, survive_plants, c_mat)
+info <- setup.plants(reproductionfunction, live_plants_live, c_mat)
 
 info
 
@@ -69,9 +73,10 @@ timesteps <-5
 plants <- array ("", dim=c(dim(t_mat), timesteps +1)) #timestep +1 allows us to record the old timestep
 plants_traveling_through_time <- function (plants, info)
 
-  #planting the garden!  If I was actually planting a garden it would be all weeds though.  And the flowers
-  #and grass would have a 0% survival chance.
-  plants[1,1,1]<- " "
+#planting the garden!  If I was actually planting a garden it would be all weeds though.  And the flowers
+#and grass would have a 0% survival chance.  Telling it where to start for time=1. Or 0.  I'm actually not sure.
+  
+plants[1,1,1]<- " "
 plants[1,2,1]<-"weed"
 plants[1,3,1]<-"flower"
 plants[2,1,1]<-" "
@@ -81,58 +86,78 @@ plants[3,3,1]<-"grass"
 plants[3,2,1]<-" "
 plants[3,1,1]<-"weed"
 
-#setting up the founatin.  Or making a puddle.  Up to you. In my life highly likely a puddle
+#setting up the fountain.  Or making a puddle.  Up to you. In my life highly likely a puddle
 
 for(k in seq_len(dim(plants)[3]))
   plants[,,k][is.na(t_mat)] <- NA
 
-survive <- function (cell, info, name){
+timesteps <-5
+#plants <- array ("", dim=c(dim(t_mat), timesteps +1)) #timestep +1 allows us to record the old timestep
+# plants_traveling_through_time <- function (plants, info){
+#   survive <- function (plant, info){
+#   }
+#   return (new.plants.matrix)
+# }
+plants_traveling_through_time <- function (plants, info){
+survival_of_the_fittest <- function (cell, info, name){
   youaretheweakestlink <- runif(1)
   if(is.na(cell))
     return(na)
   if((cell)=='')
     return(' ')
-  if(youaretheweakestlink <= info$survive[cell])
+  if(youaretheweakestlink <= info$live_plants_live[cell])
     return(name)
-  if(youaretheweakestlink >= info$survive[cell])
+  if(youaretheweakestlink >= info$live_plants_live[cell])
     return(' ')
+  #If NA = water, return NA for survival
+  #If cell is empty return ""
+  #If cell has a plant, run survival, return
+  #"a""b""c" if survives, or "" if no survival
+}
+#run-ecosystem: I like to loop it loop it
   for (k in seq_len(dim(plants)[3]-1))
     for (i in 1:nrow(plants)){
       for(j in 1:ncol(plants)){
-        plants_happy_home <- survive (plants[i,j,k], info)
-        plants[i,j,(k+1)] <- plants_happy_home
+        t_mat <- survival_of_the_fittest(plants[i,j,k], info)
+        plants[i,j,(k+1)] <- t_mat
       }
     }
   return (plants)
 }
-
-#If NA = water, return NA for survival
-#If cell is empty return ""
-#If cell has a plant, run survival, return
-#"a""b""c" if survives, or "" if no survival
+plants_traveling_through_time (plants, info)
 
 
-timesteps <-5
-plants <- array ("", dim=c(dim(t_mat), timesteps +1)) #timestep +1 allows us to record the old timestep
-plants_traveling_through_time <- function (plants, info){
-  survive <- function (plant, info){
-  }
-  return (new.plants.matrix)
-}
+#####ummmm  I don't think I actually need this.  I did everything seperately....
+#unless I need to do it within the array
 
-run_plants_run <- function (t_mat, num.timesteps, info){
-  plants <- array ("", dim=c(dim(t_mat), timesteps +1)) #timestep +1 allows us to record the old timestep in order to keep track of where in time we are
-  for (i in seq_len(dim(plants)[3]))
-    plants [,,i][is.na(t_mat)] <- NA
-  for(i in seq_len(dim(plants)[3]))
-    print(k)
-  plants[i,j,k] <- plants_traveling_through_time(plants[,,k], info)
-}
+#run_plants_run <- function (plants, info, timesteps){
+ #use my mad established planting skills
+# plants <- array ("", dim=c(dim(t_mat), timesteps +1)) #timestep +1 allows us to record the old timestep in order to keep track of where in time we are
+#  plants[1,1,1]<- " "
+#  plants[1,2,1]<-"weed"
+#  plants[1,3,1]<-"flower"
+#  plants[2,1,1]<-" "
+#  plants[2,2,1]<-"flower"
+#  plants[2,3,1]<-"grass"
+#  plants[3,3,1]<-"grass"
+#  plants[3,2,1]<-" "
+#  plants[3,1,1]<-"weed"
+  #using my mad puddle skills
+#  for(k in seq_len(dim(plants)[3]))
+#    plants[,,k][is.na(t_mat)] <- NA
+  #you can do it my creation!  LIVE!  LIVE THORUGH TIME!
+#  plants <- plants_traveling_through_time(plants, info)
+#  return(plants)
+#}
 
-run_plants_run
-plant <- reproduce(row, column, plants, info)
+#run_plants_run (plants, info, timesteps)
+
+
+######make them do things.  Like make new plants
 
 Plants_making_new_plants <- function(row, col, plants, info){
   places_youll_go <- as.matrix(expand.grid(row+c(-1,0,1), col+c(-1,0,1)))
 
   return(plants) }
+
+
--- terrain.R
+++ terrain.R
@@ -1,22 +1,169 @@
-# R world is made up of grid cells, the elements of a matrix
-#basis = terrain, will be a numeric matrix whose elements represent height
-#pick a starting height for four courners
-# need to create the corners of the matrix
-up_left <- round (rnorm (1, mean=4), digits=1)
-up_right <-round (rnorm (1, mean=4), digits=1)
-Lower_left <-round (rnorm (1, mean=4), digits=1)
-Lower_right <-round (rnorm (1, mean=4), digits=1)
-
-#add values
-mat[1,1] <- up_left
-mat[1,y] <- up_right
-mat[x,1] <- Lower_left
-mat[x,y] <- Lower_right
-
-mat
-# have corners, everythihgn else is NA
+# # R world is made up of grid cells, the elements of a matrix
+# #basis = terrain, will be a numeric matrix whose elements represent height
+# #pick a starting height for four courners
+# # need to create the corners of the matrix
+# up_left <- round (rnorm (1, mean=4), digits=1)
+# up_right <-round (rnorm (1, mean=4), digits=1)
+# Lower_left <-round (rnorm (1, mean=4), digits=1)
+# Lower_right <-round (rnorm (1, mean=4), digits=1)
+# 
+# #add values
+# mat[1,1] <- up_left
+# mat[1,y] <- up_right
+# mat[x,1] <- Lower_left
+# mat[x,y] <- Lower_right
+# 
+# mat
+# # have corners, everythihgn else is NA
 
 #make a square matrix with odd dimensions:
+mat_function <- function (n){
+  x <- (2^n)+1
+  y <- (2^n)+1
+  mat <- matrix(ncol=(2^n)+1, nrow=(2^n)+1)
+  up_left <- round (rnorm (1, mean=4), digits=1)
+  up_right <-round (rnorm (1, mean=4), digits=1)
+  Lower_left <-round (rnorm (1, mean=4), digits=1)
+  Lower_right <-round (rnorm (1, mean=4), digits=1)
+  mat[1,1] <- up_left
+  mat[1,y] <- up_right
+  mat[x,1] <- Lower_left
+  mat[x,y] <- Lower_right
+  return(mat)
+}
+
+mat <- mat_function(2)
+mat
+#x <- n^2+1 , so 5, 9
+#y <- n^2+1 , so 5, 9
+
+
+#repeat Diamond step, square step until matrix is filled with values
+
+diamond_step <- function (mat,n){
+  x <- (2^n)+1
+  y <- (2^n)+1
+  up_l <- mat [1,1]
+  up_r <- mat [1,y]
+  low_left <- mat [x,1]
+  low_right <- mat [x,y]
+  mean_vector <- c(up_l, up_r, low_left, low_right)
+  mean_corners <- mean(mean_vector)
+  median_x <- median(1:x)
+  median_y <- median (1:y)
+  mat[median_y, median_x] <- mean_corners
+  return(mat)
+}
+mat <- diamond_step(mat,2)
+mat
+#Yay!  Now we also have one value in the middle!
+
+
+
+# square_step <-
+
+
+square_step <- function (mat, n){
+  x <- (2^n)+1
+  y <- (2^n)+1
+  median_x <- median(1:x)
+  median_y <- median (1:y)
+  mat[median_y, median_x] <- mean(c(mat[median_x,y], mat[median_x,1], mat[1,median_y], mat[median_x,y]))
+  mat[1,median_y] <- mean(c(mat [1,1], mat [x,1],mat[median_y, median_x]), na.rm=TRUE)
+  mat[x,median_y] <- mean(c(mat [1,y],mat [1,y],mat[median_y, median_x]), na.rm=TRUE)
+  mat[median_x,y] <- mean(c(mat [x,y],mat [1,y],mat[median_y, median_x]), na.rm=TRUE)
+  mat[median_x,1] <- mean(c(mat [x,y],mat [x,1],mat[median_y, median_x]), na.rm=TRUE)
+  return(mat)
+}
+
+mat <- square_step(mat, 2)
+mat
+
+# diamond_square_step <- #needs to call both former functions in turn until the matrix
+#is filled up, needs to initialize everything - seed w/starting values, create matrix, etc.
+#should step through matrix, affecting smaller and smaller chunks
+#Add a little bit of noise with rnorm that decreases with each iteration (sd argument)
+
+# diamond_square_step.fail <- function (mat){
+#   mat_function <- function (x, y){
+#     mat <- matrix(ncol=x, nrow=y)
+#     return(mat)
+#   }
+#   mat <- mat_function(9,9)
+#   x <- ncol(mat)
+#   y <- nrow(mat)
+#   median_x <- median(1:x)
+#   median_y <- median (1:y)
+#   mean_vector <- c(mat [1,1], mat [1,y], mat [x,1], mat [x,y])
+#   mean_corners <- mean(mean_vector)
+#   mat[median_y, median_x] <- mean(c(mat[median_x,y], mat[median_x,1], mat[1,median_y], mat[median_x,y]))
+#   mat[1,median_y] <- mean(c(mat [1,1], mat [x,1],mat[median_y, median_x]), na.rm=TRUE)
+#   mat[x,median_y] <- mean(c(mat [1,y],mat [1,y],mat[median_y, median_x]), na.rm=TRUE)
+#   mat[median_x,y] <- mean(c(mat [x,y],mat [1,y],mat[median_y, median_x]), na.rm=TRUE)
+#   mat[median_x,1] <- mean(c(mat [x,y],mat [x,1],mat[median_y, median_x]), na.rm=TRUE)
+#   return(mat)
+# }
+# # doesn't work even once - need to make it create matrix, run diamond step, run square step,
+# #then need to figure out how to loop it for smaller and smaller squares,
+# #not just one big outside square
+# 
+# mat <- diamond_square_step.fail (mat)
+# 
+# mat
+
+
+#write a function called make_terrain that will act as a wrapper for the diamond step algorithm
+#This should be adding in water as well (height values of NA)
+
+#this works...now I need to add the loop
+
+#need to loop smaller and smaller
+#seq (1,9,8)
+#mat <- matrix (1:81) 9x9 has 81 numbers
+#need to do everything in quarters
+#mat[1:5,1:5] = quarters of the 9X9
+#seq(1,9 by=8) will get numbers, 1,9
+#seq(1,9 by=4) will get numbers 1,5,9
+#seq(1,9, by=2) will get odd numbers
+#seq(1,9 by=4)[2] will get 5-9
+#changing the by # in the loop across all the columns and rows
+#but I don't want to have to tell it how big my matrix is
+#I want it to look at the matrix I have entered and work accordingly
+#I don't know how to do that for the by piece of it - I want to change the by not manually, but based on what the matrix size is
+
+Diamond_square_step_dont_fail <- function(mat,n){
+  #defining i and j so that the grid has defined odd sides. This was an attempt to bypass the issues I was having with only being able to get grids for 5 and 9 length. 
+  i <- (2^n)-1
+  j <- (2^n)-1
+  #creating a matrix
+  mat <- mat_function (n)
+  #applying my square step
+  mat <- square_step (mat,n)
+  #applying my diamond step
+  mat <- diamond_step (mat,n)
+  #defining my by variable as something that will change as it loops. So if n was two, this would give me by 4,2,1 all minus one, to keep it within the dimensions of the matrix. so 3 and 1. 
+  for (k in 2^(n:1)-1)
+    #defining my i and j again. I know I only need to do it once, I am trying still to figure out which is correct
+    for (i in seq(from=1, to=(ncol(mat)), by=k-1)){
+      for (j in seq(from=1, to=(nrow(mat)), by=k-1)){
+        #Applying my functions across the matrix. Right now it is i or j, 1:j+k-1 which is out of the dimensions because i or j + k would be too large, but it worked when I used it for 5 or 9 again, so I am just trying to figure out that piece. 
+        mat[j:(j+k-1), i:(i+k-1)] <- square_step(mat[j:(j+k-1), i:(i+k-1)],n)
+        mat[j:(j+k-1), i:(i+k-1)] <- diamond_step(mat[j:(j+k-1), i:(i+k-1)],n)
+      }
+    }
+  return(mat)
+}
+
+mat <- Diamond_square_step_dont_fail (mat,2)
+mat
+
+
+
+# I have made sure like 5 times that there is no where I am going out of bounds.
+#I know the issue is within my square step and diamond step but I can't quite get it in my head what the issue is
+
+image(mat)
+
 mat_function <- function (x, y){
   mat <- matrix(ncol=x, nrow=y)
   up_left <- round (rnorm (1, mean=4), digits=1)
@@ -31,8 +178,6 @@ mat_function <- function (x, y){
 }
 
 mat <- mat_function(5,5)
-x <- 5
-y <- 5
 mat
 #x <- n^2+1 , so 5, 9
 #y <- n^2+1 , so 5, 9
@@ -119,7 +264,7 @@ Diamond_square_step_dont_fail <- function(mat){
   mat <- mat_function (5,5)
   mat <- square_step (mat)
   mat <- diamond_step (mat)
-  }
+}
 }
 mat <- Diamond_square_step_dont_fail (mat)
 
@@ -149,8 +294,8 @@ Diamond_square_step_dont_fail <- function(mat,x,y){
     mat[j:(j+2), i:(i+2)] <- square_step(mat[j:(j+2), i:(i+2)])
     mat[j:(j+2), i:(i+2)] <- diamond_step(mat[j: (j+2), i:(i+2)])
     for (j in seq(from=1, to=(nrow(mat)-1), by=2^(4:1))){
-    mat[j:(j+2), i:(i+2)] <- square_step(mat[j:(j+2), i:i(i+2)])
-    mat[j:(j+2), i:(i+2)] <- diamond_step(mat[j:(j+2), i:(i+2)])
+      mat[j:(j+2), i:(i+2)] <- square_step(mat[j:(j+2), i:i(i+2)])
+      mat[j:(j+2), i:(i+2)] <- diamond_step(mat[j:(j+2), i:(i+2)])
     }
   }
   return(mat)
@@ -177,5 +322,3 @@ Diamond_square_step_dont_fail <- function(mat,x,y){
 }
 
 mat <- Diamond_square_step_dont_fail (mat, x=5,y=5)
-
-mat
